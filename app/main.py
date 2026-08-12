2eb636f0a9e4872b80a157c2a820afcb66b6f05a
Jesus <jiesusdance@gmail.com>
2026-08-12 12:25:05 +0300

    refactor: added init settings in lifespan with DI

--- app/main.py
+++ app/main.py
@@ -4,24 +4,25 @@ from fastapi import FastAPI
 from httpx import AsyncClient
 from redis.asyncio import Redis
 
-from app.config.config import settings
-from app.db.session import engine
-from app.models import Base
 from app.api.routers.place import router as place_router
 from app.api.routers.project import router as project_router
 from app.api.routers.users import router as user_router
+from app.config.config import get_settings
 
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+#Запуст таблиць через алембік, тому опрокидувати в лафйспен не потрібно створення таблиць
+# async def create_db_and_tables():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    await create_db_and_tables()
     app.state.httpx_client = AsyncClient(http2=True)
-    app.state.redis_client = Redis.from_url(settings.REDIS_URL, decode_responses=True)
+    app.state.settings = get_settings()
+    app.state.redis_client = Redis.from_url(
+        app.state.settings.REDIS_URL, decode_responses=True
+    )
     yield
     await app.state.httpx_client.aclose()
     await app.state.redis_client.aclose()
