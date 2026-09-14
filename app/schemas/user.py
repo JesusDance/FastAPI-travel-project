d0a18fd24236885ad79dd516798be65f2b2c31c9
Jesus <jiesusdance@gmail.com>
2026-09-14 17:12:05 +0300

    refactor: added user sign_up/login schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
 
 
-class UserIn(BaseModel):
+class UserSignUp(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    password: str = Field(min_length=8, max_length=255)
+    email: EmailStr
+
+
+class UserLogin(BaseModel):
     username: str = Field(min_length=3, max_length=50)
     password: str = Field(min_length=8, max_length=255)
     email: EmailStr | None = None
