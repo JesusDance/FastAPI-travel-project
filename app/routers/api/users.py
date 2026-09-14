3959c20e035f0a4c968f7693dab07cfbdba17538
Jesus <jiesusdance@gmail.com>
2026-09-14 17:05:37 +0300

    added schemas for sign_up and user_login plus exceptions

--- app/routers/api/users.py
+++ app/routers/api/users.py
@@ -1,8 +1,9 @@
 from typing import Annotated, Any
 
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, status
 from fastapi.exceptions import HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, or_
+from sqlalchemy.exc import IntegrityError
 
 from app.core.security import verify_password, create_access_token, \
     get_password_hash
@@ -10,39 +11,48 @@ from app.db.session import SessionDep
 from app.models.user import User
 from app.routers.dependencies import SettingsDep
 from app.schemas.token import Token
-from app.schemas.user import UserIn, UserOut
+from app.schemas.user import UserSignUp, UserLogin, UserOut
 
 router = APIRouter(prefix="/register", tags=["register"])
-USER = Annotated[UserIn, Body()]
+USER_SIGN_UP = Annotated[UserSignUp, Body()]
+USER_LOGIN = Annotated[UserLogin, Body()]
 
 
 @router.post("/", response_model=UserOut, status_code=201)
-async def register_user(session: SessionDep, user: USER) -> Any:
+async def register_user(session: SessionDep, user: USER_SIGN_UP) -> Any:
     existing_user = await session.scalar(
-        select(User).where(User.username == user.username)
+        select(User).where(
+            or_(User.username == user.username, User.email == user.email)
+        )
     )
 
     if existing_user:
-        raise HTTPException(400, "User already exists")
-    if not user:
-        raise HTTPException(422)
+        raise HTTPException(
+            status.HTTP_409_CONFLICT, "Username or email already exists"
+        )
 
     user_db = User(
         **user.model_dump(exclude={"password"}),
         password=get_password_hash(user.password)
     )
     session.add(user_db)
-    await session.commit()
-    await session.refresh(user_db)
+    try:
+        await session.commit()
+        await session.refresh(user_db)
+    except IntegrityError:
+        await session.rollback()
+        raise HTTPException(
+            status.HTTP_409_CONFLICT, "Username or email already exists"
+        )
 
     return user_db
 
 
 @router.post("/login", response_model=Token)
-async def login_user(session: SessionDep, user: USER, settings: SettingsDep) -> Any:
+async def login_user(session: SessionDep, user: USER_LOGIN, settings: SettingsDep) -> Any:
     existing_user = await session.scalar(
         select(User).where(User.username == user.username)
-    )
+        )
 
     if not existing_user or not verify_password(user.password, existing_user.password):
         raise HTTPException(401, "Invalid username or password")
